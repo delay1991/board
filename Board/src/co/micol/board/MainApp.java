4f0d65d3394f1b9330de298ed98d855cbacfca82
Admin <Admin@DESKTOP-5KUCMGU>
2021-07-15 15:10:34 +0900

    board 과제

---
+++ Board/src/co/micol/board/MainApp.java
@@ -0,0 +1,9 @@
+package co.micol.board;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Board/src/co/micol/board/vo/BoardVO.java
@@ -0,0 +1,67 @@
+package co.micol.board.vo;
+
+import java.util.Date;
+
+public class BoardVO {
+	private String boardId;
+	private String writer;
+	private String title;
+	private String subject;
+	private Date date;
+	private int hit;
+	
+	public BoardVO() { //jvm이 인자없으면 알아서 만들어줌~
+	}
+
+	public String getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(String boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	
+	
+	
+	
+}
